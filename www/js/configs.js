6732a7748187bf1ce1abee0655952621f3afa333
lucas honda <lucas.honda@gzsistemas.com.br>
2017-06-27 13:55:32 -0300

    MFLASH-3 MFLASH-4 MFLASH-5 #comment Atualizado o toast e toda a funcionalidade de login #done

--- www/js/configs.js
+++ www/js/configs.js
@@ -1,37 +1,125 @@
 /**
  *
- *  configs.js
+ *  Configs.js
  *
- * última atualização 10/05/2017
  */
  var conexaoSegura = false;
+
+ $('.toggles').toggles({text:{on:'SIM', off:'NÃO'}});
 
  $('#tgl-conexao-segura').on('toggle', function(e, active) {
  	conexaoSegura = active;
  });
 
- $('.toggles').toggles({text:{on:'SIM', off:'NÃO'}});
 /*
   Definições das funções
  */
 function onLoad() {
   var storage = window.localStorage;
-  conexaoSegura = JSON.parse(storage.getItem("ConecSeg"));
+  conexaoSegura = getSSL();
+  var status = getStatus();
 
   $('#tgl-conexao-segura').toggles({
 		on: conexaoSegura
 	});
+
+  $('.carousel').carousel({
+    interval: false
+  });
+
+  if(status == true){
+    var usr = getUser();
+    var senha = getSenha();
+    var empresa = getEmpresa();
+    $("#txt-usuario-cloud").val(usr);
+    $("#txt-senha-cloud").val(senha);
+    $("#txt-empresa").val(empresa);
+  } else{
+    var usr = getUser();
+    var senha = getSenha();
+    var empresa = getEmpresa();
+    var url = getUrlbase();
+    $("#txt-usuario").val(usr);
+    $("#txt-senha").val(senha);
+    $("#txt-empresa").val(empresa);
+    $("#txt-url-base").val(url);
+  }
+
 }
 
+function guardar(){
+  var user = $("#txt-usuario-cloud").val();
+  var senha = $("#txt-senha-cloud").val();
+  var empresa = $("#txt-empresa").val();
+  var url = "http://homologacao.gzcloud.com.br/flex-e";
+  var status = true;
+  setUser(user);
+  setSenha(senha);
+  setEmpresa(empresa);
+  setUrlbase(url);
+  setStatus(status);
+  toastInfoNoHide("Configurações salvas!");
+}
 
 function salvar() {
-  var storage = window.localStorage;
-  //alert(conexaoSegura);
-  storage.setItem("ConecSeg", JSON.stringify(conexaoSegura));
-  toastInfoNoHide("Configurações salvas!");
-  window.location.replace("login.html");
+  if($("#txt-empresa").val() == ""){
+    toastError("Empresa / Loja não pode ser nula!");
+  } else {
+    if($("#txt-usuario-cloud").val() == "" | $("#txt-senha-cloud").val() == ""){
+      if($("#txt-usuario").val() == "" | $("#txt-senha").val() == "" | $("#txt-url-base").val() == ""){
+        toastError("Existem campos em branco!");
+      } else{
+        var user = $("#txt-usuario").val();
+        var senha = $("#txt-senha").val();
+        var empresa = $("#txt-empresa").val();
+        var url = $("#txt-url-base").val();
+        var status = false;
+        setUser(user);
+        setSenha(senha);
+        setEmpresa(empresa);
+        setUrlbase(url);
+        setStatus(status);
+        setSSL(conexaoSegura);
+        toastInfoNoHide("Configurações salvas!");
+        login();
+      }
+    } else {
+      var storage = window.localStorage;
+      var user = JSON.parse(storage.getItem("user"));
+      if(user == null){
+        guardar();
+        login();
+      } else{
+        confirmar();
+      }
+    }
+  }
 }
 
+function confirmar() {
+  bootbox.confirm({
+      message: "Utilizar dados Cloud ?",
+      size: "small",
+      buttons: {
+          confirm: {
+              label: 'Sim',
+              className: 'btn-success'
+          },
+          cancel: {
+              label: 'Não',
+              className: 'btn-danger'
+          }
+      },
+      callback: function (result) {
+        if (result == true) {
+          guardar();
+          login();
+        } else {
+          login();
+        }
+      }
+  });
+}
 
 /*
   Definições da funções que os botões irão chamar
